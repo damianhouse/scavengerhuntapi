22144145494f17b9283c6ce6eb740069eb2bf8ec
damianhouse <damianhouse@gmail.com>
2017-08-06 17:34:54 -0400

    Updated scaffolded routes to be namespaced

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  resources :scores
-  resources :answers
-  resources :questions
-  resources :players
-  resources :teams
-  resources :games
   mount_devise_token_auth_for 'User', at: 'auth'
 
   scope module: 'api' do
     namespace :v1 do
       resources :users
+      resources :scores
+      resources :answers
+      resources :questions
+      resources :players
+      resources :teams
+      resources :games
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
