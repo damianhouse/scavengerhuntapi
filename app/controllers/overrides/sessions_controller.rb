50d90c764b87071e8a8a9aac5d055cb4aaee2851
damianhouse <damianhouse@gmail.com>
2017-08-23 23:32:21 -0400

    Refactored the user model to give a hash of the last player, game, and team ids. Updated sessions and token validation overrides

--- app/controllers/overrides/sessions_controller.rb
+++ app/controllers/overrides/sessions_controller.rb
@@ -2,7 +2,7 @@ module Overrides
   class SessionsController < DeviseTokenAuth::SessionsController
     def render_create_success
       render json: {
-        data: @resource.as_json(methods: [:last_team, :last_player])
+        data: @resource.as_json(methods: :last_team_player_and_game_ids)
       }
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,16 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   before_save :downcase_email
 
-  def last_team
-    Player.where(user: self) != [] && Player.where(user: self).last.team ? Player.where(user: self).last.team : nil
-  end
-
-  def last_player
-    Player.where(user: self) != [] ? Player.where(user: self).last : nil
+  def last_team_player_and_game_ids
+    last_team = Player.where(user: self) != [] && Player.where(user: self).last.team ? Player.where(user: self).last.team : nil
+    last_team_id = last_team ? last_team.id : nil
+    last_game_id = last_team ? last_team.game_id : nil
+    last_player_id = Player.where(user: self) != [] ? Player.where(user: self).last.id : nil
+    {
+      last_team_id: last_team_id, 
+      last_player_id: last_player_id,
+      last_game_id: last_game_id,
+    }
   end
 
   def downcase_email
