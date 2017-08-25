7c919130859533423dca18597adc20f72312fc67
damianhouse <damianhouse@gmail.com>
2017-08-24 21:29:16 -0400

    Added answers to the team serializer

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,6 +1,3 @@
 class TeamSerializer < ActiveModel::Serializer
-  attributes :id, :game_id, :score, :name, :type, :questions
-  class AnswerSerializer < ActiveModel::Serializer
-    attributes :id, :question_id, :player_id, :image_url, :lat, :long, :answerText, :hasItem, :verified
-  end
+  attributes :id, :game_id, :score, :name, :type, :questions, :answers  
 end
