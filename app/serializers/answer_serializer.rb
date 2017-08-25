d1d6080b6046ea1df7837b5204f45c332a3222f8
damianhouse <damianhouse@gmail.com>
2017-08-24 21:18:59 -0400

    Added associations in the serializers

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,3 +1,4 @@
 class AnswerSerializer < ActiveModel::Serializer
   attributes :id, :question_id, :player_id, :image_url, :lat, :long, :answerText, :hasItem, :verified
+  belongs_to :team
 end
--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,7 +1,4 @@
 class TeamSerializer < ActiveModel::Serializer
   attributes :id, :game_id, :score, :name, :type, :questions
   has_many :answers, through: :players
-  class AnswerSerializer < ActiveModel::Serializer
-    attributes :id, :question_id, :player_id, :image_url, :lat, :long, :answerText, :hasItem, :verified
-  end
 end
