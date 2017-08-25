1f1ccacb13893e920f97e4f93e90a209bb152821
damianhouse <damianhouse@gmail.com>
2017-08-24 20:50:44 -0400

    Adjusted team serializer for answers

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,5 +1,6 @@
 class TeamSerializer < ActiveModel::Serializer
-  attributes :id, :game_id, :score, :name, :questions, :answers
+  attributes :id, :game_id, :score, :name, :questions
+  has_many :answers
   class AnswerSerializer < ActiveModel::Serializer
     attributes :id, :question_id, :player_id, :image_url, :lat, :long, :answerText, :hasItem, :verified
   end
