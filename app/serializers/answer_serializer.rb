ef7472dfd39e9370d4a3a10f9843f1e34333fd80
damianhouse <damianhouse@gmail.com>
2017-08-24 21:26:43 -0400

    Removed associations from within the serializers and added the answers serializer to the team

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,4 +1,3 @@
 class AnswerSerializer < ActiveModel::Serializer
   attributes :id, :question_id, :player_id, :image_url, :lat, :long, :answerText, :hasItem, :verified
-  belongs_to :team
 end
--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,4 +1,6 @@
 class TeamSerializer < ActiveModel::Serializer
   attributes :id, :game_id, :score, :name, :type, :questions
-  has_many :answers, through: :players
+  class AnswerSerializer < ActiveModel::Serializer
+    attributes :id, :question_id, :player_id, :image_url, :lat, :long, :answerText, :hasItem, :verified
+  end
 end
