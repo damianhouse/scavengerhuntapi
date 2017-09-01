6552731ca016d4d3427284bf293e10be17985c25
damianhouse <damianhouse@gmail.com>
2017-09-01 13:38:58 -0400

    Removed the suming of scores for teamsj

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,15 +2,5 @@ class Score < ApplicationRecord
     belongs_to :answer
     belongs_to :team
     validates :team_id, :answer_id, presence: true
-    after_save :update_team_score
-    before_destroy do
-        self.team.score -= self.score
-        self.team.save!
-    end
-
-    private
-    def update_team_score
-        self.team.score = Score.where(team: self.team).sum(:score)
-        self.team.save!
-    end
+    
 end
