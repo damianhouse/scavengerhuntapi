1e23b8a2f94261409601db685a9f2cf6de46b609
damianhouse <damianhouse@gmail.com>
2017-09-03 13:37:25 -0400

    Refactored update_verified method and applied to before_destroy as well

--- app/models/score.rb
+++ app/models/score.rb
@@ -3,15 +3,11 @@ class Score < ApplicationRecord
     belongs_to :team
     validates :team_id, :answer_id, presence: true
     after_save :update_verified
-
-    before_destroy do
-        self.answer.verified = false
-        self.answer.save!
-    end
+    before_destroy :update_verified
 
     private
     def update_verified
-        self.answer.verified = true
+        self.answer.verified = !self.answer.verified
         self.answer.save!
     end
 end
