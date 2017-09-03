dd9303c6d69427dd3c465afdb0c842b9f81bf950
damianhouse <damianhouse@gmail.com>
2017-09-03 14:06:12 -0400

    Remove space

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -8,8 +8,7 @@ class Answer < ApplicationRecord
 	# This method associates the attribute ":image" with a file attachment
 	has_attached_file :image, styles: {
 			original: {convert_options: '-auto-orient'}
-	} 
-
+	}
 	# Validate the attached image is image/jpg, image/png, etc
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
