38a0ae7a08902670b7a43cfd1116332510ee32d0
damianhouse <damianhouse@gmail.com>
2017-09-03 13:33:59 -0400

    When a score is created/destroyed the answer verified is set to true/false

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,5 +2,16 @@ class Score < ApplicationRecord
     belongs_to :answer
     belongs_to :team
     validates :team_id, :answer_id, presence: true
-    
+    after_save :update_verified
+
+    before_destroy do
+        self.answer.verified = false
+        self.answer.save!
+    end
+
+    private
+    def update_verified
+        self.answer.verified = true
+        self.answer.save!
+    end
 end
