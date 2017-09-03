63ad1021e0a965b224f195e1629727e79e76633a
damianhouse <damianhouse@gmail.com>
2017-09-03 08:00:11 -0400

    Fix photo orientation when coming from a phone

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -10,8 +10,9 @@ class Answer < ApplicationRecord
 	has_attached_file :image, styles: {
 			thumb: '100x100>',
 			square: '200x200#',
-			medium: '300x300>'
-	}
+			medium: '300x300>',
+			original: {convert_options: '-auto-orient'}
+	} 
 
 	# Validate the attached image is image/jpg, image/png, etc
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
